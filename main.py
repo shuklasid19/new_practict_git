e41a9677385965cecf316a7d510063b30826c4d4
shuklasid19 <shuklasid19@gmail.com>
2023-01-03 07:47:47 +0530

    new changes made

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def square(x, y):
+    return x*y
 
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(square(2,7))
 
 print("control the git files using add, commit, push ")
